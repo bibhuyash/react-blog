f5c9ab8d03d7a5467ea78f25ccf959f3c34e7d77
bibhuyash <vibhakaryashaswi@gmail.com>
2020-01-25 11:49:39 +0530

    committing changes

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb + srv: //bibhuyash:Avichal.27@cluster0-i1ot5.mongodb.net/test?retryWrites=true&w=majority"
+
+}
--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const {
+  User
+} = require("./model/user");
 
 const app = express();
 mongoose
   .connect(
-    "mongodb+srv://bibhuyash:Avichal.27@cluster0-i1ot5.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
   )
   .then(() => console.log("DB is connected Successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userdata) => {
+    if (err) {
+      return res.json({
+        sucess: false,
+        err
+      });
+    } else {
+      return res.status(200).json({
+        sucess: true
+      });
+    }
+  });
+});
 
 app.listen(5000);
